ab326eade020a3565bca0fc18a2e77b130575eb9
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-27 18:46:35 +0200

    Add tse changes, static file

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom';
 import './CSS/index.scss';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { v4 as uuidv4 } from 'uuid';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import BookReducer from './reducers/books';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import initialState from './staticData';
 
-const initialState = {
-  books: [
-    { id: uuidv4(), title: 'The Lord of the Rings', category: 'Fiction' },
-    { id: uuidv4(), title: 'The Return of the King', category: 'Fiction' },
-  ],
-};
 const store = createStore(BookReducer, initialState);
 ReactDOM.render(
   <React.StrictMode>

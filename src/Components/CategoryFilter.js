3936070fb8b42c2633b86d9de2675a45af82d25a
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-28 18:10:36 +0200

    Add linters fix

--- src/Components/CategoryFilter.js
+++ src/Components/CategoryFilter.js
@@ -4,7 +4,7 @@ import Select from 'react-select';
 const Selector = ({ handleOnChange, options }) => (
   <Select options={options} onChange={(e) => handleOnChange(e)} />
 );
- 
+
 Selector.propTypes = {
   handleOnChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(PropTypes.object).isRequired,

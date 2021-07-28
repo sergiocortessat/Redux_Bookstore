3ebabf7b4692cf52c800590f19dc056ffb43a738
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-28 18:06:47 +0200

    Add removed all inline comments

--- src/Components/CategoryFilter.js
+++ src/Components/CategoryFilter.js
@@ -1,7 +1,12 @@
-/* eslint-disable react/prop-types */
+import { PropTypes } from 'prop-types';
 import Select from 'react-select';
 
 const Selector = ({ handleOnChange, options }) => (
   <Select options={options} onChange={(e) => handleOnChange(e)} />
 );
+ 
+Selector.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default Selector;

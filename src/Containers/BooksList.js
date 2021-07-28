e723a076937e898c0707ac68c7324c2deb153853
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-28 12:31:36 +0200

    Add filter logic

--- src/Containers/BooksList.js
+++ src/Containers/BooksList.js
@@ -1,24 +1,33 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from '../Components/Book';
 import { removeBook } from '../actions';
+import Select from '../Components/CategoryFilter';
 
 const BooksList = () => {
   const { books } = useSelector((state) => state.books);
+  const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
   const handleRemoveBook = (book) => {
     dispatch(removeBook(book));
   };
-  const bookMapping = books.map((book) => (
-    <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
-  ));
 
-  const getFilteredBooks = ({ books, filter }) => {
-    if (filter === 'All') {
-      return books;
+  // const bookMapping = books.map((book) => (
+  //   <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
+  // ));
+
+  const getFilteredBooks = () => {
+    if (filter === 'ALL') {
+      return books.map((book) => (
+        <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
+      ));
     }
-    return books.filter(({ category }) => category === filter);
+    return books.filter(({ category }) => category === filter.filter)
+      .map((book) => (
+        <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
+      ));
   };
 
   return (
@@ -33,9 +42,10 @@ const BooksList = () => {
           </tr>
         </thead>
         <tbody>
-          {bookMapping}
+          {getFilteredBooks()}
         </tbody>
       </table>
+      <Select />
     </div>
   );
 };

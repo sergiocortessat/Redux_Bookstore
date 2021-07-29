251f8d91dc4dd4a1d98ea01b875822d236966e59
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-29 16:42:46 +0200

    Add more initial books

--- src/staticData.js
+++ src/staticData.js
@@ -2,8 +2,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
   books: [
-    { id: uuidv4(), title: 'The Lord of the Rings', category: 'Horror' },
-    { id: uuidv4(), title: 'The Return of the King', category: 'Sci-Fi' },
+    { id: uuidv4(), title: 'The Lord of the Rings - Return of the king', category: 'Horror' },
+    { id: uuidv4(), title: 'The Lord of the Rings - The two towers', category: 'Sci-Fi' },
+    { id: uuidv4(), title: 'The Lord of the Rings - The fellowship of the ring', category: 'Sci-Fi' },
+    { id: uuidv4(), title: '100 years if solitude', category: 'Sci-Fi' },
+    { id: uuidv4(), title: 'How to make friends and influence people', category: 'Education' },
+    { id: uuidv4(), title: 'The Shining', category: 'Biography' },
   ],
 };
 
